94046899e558cf171f185aa1d326a3a53ac707ef
Henrique <henriqueweiss@hotmail.com>
2024-11-21 12:43:33 -0300

    feat: added test for save-recieved-message

--- packages/jobs/schemas/received-messages.schema.ts
+++ packages/jobs/schemas/received-messages.schema.ts
@@ -15,3 +15,29 @@ export const querySchema = z
       data.toTimestamp >= data.fromTimestamp,
     { message: "toTimestamp must be greater than or equal to fromTimestamp" },
   );
+
+export const messageSchema = z.object({
+  id: z.string(),
+  _id: z.string(),
+  from: z.string(),
+  to: z.string(),
+  userId: z.string(),
+  data: z.object({
+    text: z.string(),
+  }),
+  timestamp: z.number(),
+  type: z.string(),
+  isGroupChat: z.boolean(),
+  chatId: z.string(),
+  created: z.string(),
+});
+
+export type MessageResult = {
+  id: string;
+  user_id: string;
+  text: string;
+  timestamp: number;
+  processed: boolean;
+  meta: Record<string, unknown>;
+  created_at: string;
+};
