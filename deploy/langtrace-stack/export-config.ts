1deaa50e77af29caf8f4d641a4b1fbc2bee5e166
Luis Otavio <luisotaviomgr1@gmail.com>
2025-01-22 00:35:26 -0300

    Refactor: Update langtrace deployment to use a script for environment variable generation

---
+++ deploy/langtrace-stack/export-config.ts
@@ -0,0 +1,51 @@
+//@ts-ignore
+import { config } from "@eda/config";
+
+const envVars = {
+  // Core settings
+  PORT: config.ports.langtrace,
+
+  // Postgres settings
+  POSTGRES_HOST: config.databases.langtrace_postgres.host,
+  POSTGRES_USER: config.databases.langtrace_postgres.user,
+  POSTGRES_PASSWORD: config.databases.langtrace_postgres.password,
+  POSTGRES_DATABASE: config.databases.langtrace_postgres.database,
+  POSTGRES_DB: config.databases.langtrace_postgres.database,
+  POSTGRES_URL: `postgres://${config.databases.langtrace_postgres.user}:${config.databases.langtrace_postgres.password}@${config.databases.langtrace_postgres.host}/${config.databases.langtrace_postgres.database}`,
+  POSTGRES_PRISMA_URL: `postgres://${config.databases.langtrace_postgres.user}:${config.databases.langtrace_postgres.password}@${config.databases.langtrace_postgres.host}/${config.databases.langtrace_postgres.database}?pgbouncer=true&connect_timeout=15`,
+  POSTGRES_URL_NO_SSL: `postgres://${config.databases.langtrace_postgres.user}:${config.databases.langtrace_postgres.password}@${config.databases.langtrace_postgres.host}/${config.databases.langtrace_postgres.database}`,
+  POSTGRES_URL_NON_POOLING: `postgres://${config.databases.langtrace_postgres.user}:${config.databases.langtrace_postgres.password}@${config.databases.langtrace_postgres.host}/${config.databases.langtrace_postgres.database}`,
+  POSTGRES_IMAGE_TAG: "16",
+
+  // App settings
+  NEXT_PUBLIC_APP_NAME: config.services.langtrace.api.host,
+  NEXT_PUBLIC_ENVIRONMENT: "production",
+  NEXT_PUBLIC_HOST: "http://localhost:3000",
+  NEXTAUTH_SECRET: config.services.dashboard.auth.nextauth_secret,
+  NEXTAUTH_URL: "http://localhost:3000",
+  NEXTAUTH_URL_INTERNAL: "http://localhost:3000",
+
+  // Clickhouse settings
+  CLICK_HOUSE_HOST: config.databases.langtrace_clickhouse.host,
+  CLICK_HOUSE_USER: config.databases.langtrace_clickhouse.user,
+  CLICK_HOUSE_PASSWORD: config.databases.langtrace_clickhouse.password,
+  CLICK_HOUSE_DATABASE_NAME: config.databases.langtrace_clickhouse.database,
+
+  // Admin settings
+  ADMIN_EMAIL: config.services.langtrace.admin.email,
+  ADMIN_PASSWORD: config.services.langtrace.admin.password,
+  NEXT_PUBLIC_ENABLE_ADMIN_LOGIN: config.services.langtrace.admin.enable_login,
+
+  // Azure AD settings
+  AZURE_AD_CLIENT_ID: config.services.dashboard.auth.azure.client_id,
+  AZURE_AD_CLIENT_SECRET: config.services.dashboard.auth.azure.client_secret,
+  AZURE_AD_TENANT_ID: config.services.dashboard.auth.azure.tenant_id,
+
+  // Additional settings
+  POSTHOG_HOST: config.services.langtrace.posthog.host,
+  TELEMETRY_ENABLED: config.services.langtrace.telemetry.enabled,
+};
+
+for (const [key, value] of Object.entries(envVars)) {
+  console.log(`${key}=${value}`);
+}
