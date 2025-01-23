b56cf03c84d3f4b1930fd16e8ef922c5030554ac
Luis Otavio <luisotaviomgr1@gmail.com>
2025-01-23 08:53:03 -0300

    Fixed langtrace configuration

--- deploy/langtrace-stack/export-config.ts
+++ deploy/langtrace-stack/export-config.ts
@@ -6,25 +6,25 @@ const envVars = {
   PORT: config.ports.langtrace,
 
   // Postgres settings
-  POSTGRES_HOST: config.databases.langtrace_postgres.host,
+  POSTGRES_HOST: `${config.databases.langtrace_postgres.host}:5432`, // Add port to host
   POSTGRES_USER: config.databases.langtrace_postgres.user,
   POSTGRES_PASSWORD: config.databases.langtrace_postgres.password,
   POSTGRES_DATABASE: config.databases.langtrace_postgres.database,
   POSTGRES_DB: config.databases.langtrace_postgres.database,
-  POSTGRES_URL: `postgres://${config.databases.langtrace_postgres.user}:${config.databases.langtrace_postgres.password}@${config.databases.langtrace_postgres.host}/${config.databases.langtrace_postgres.database}`,
-  POSTGRES_PRISMA_URL: `postgres://${config.databases.langtrace_postgres.user}:${config.databases.langtrace_postgres.password}@${config.databases.langtrace_postgres.host}/${config.databases.langtrace_postgres.database}?pgbouncer=true&connect_timeout=15`,
-  POSTGRES_URL_NO_SSL: `postgres://${config.databases.langtrace_postgres.user}:${config.databases.langtrace_postgres.password}@${config.databases.langtrace_postgres.host}/${config.databases.langtrace_postgres.database}`,
-  POSTGRES_URL_NON_POOLING: `postgres://${config.databases.langtrace_postgres.user}:${config.databases.langtrace_postgres.password}@${config.databases.langtrace_postgres.host}/${config.databases.langtrace_postgres.database}`,
+  POSTGRES_URL: `postgres://${config.databases.langtrace_postgres.user}:${config.databases.langtrace_postgres.password}@${config.databases.langtrace_postgres.host}:5432/${config.databases.langtrace_postgres.database}`,
+  POSTGRES_PRISMA_URL: `postgres://${config.databases.langtrace_postgres.user}:${config.databases.langtrace_postgres.password}@${config.databases.langtrace_postgres.host}:5432/${config.databases.langtrace_postgres.database}?pgbouncer=true&connect_timeout=15`,
+  POSTGRES_URL_NO_SSL: `postgres://${config.databases.langtrace_postgres.user}:${config.databases.langtrace_postgres.password}@${config.databases.langtrace_postgres.host}:5432/${config.databases.langtrace_postgres.database}`,
+  POSTGRES_URL_NON_POOLING: `postgres://${config.databases.langtrace_postgres.user}:${config.databases.langtrace_postgres.password}@${config.databases.langtrace_postgres.host}:5432/${config.databases.langtrace_postgres.database}`,
   POSTGRES_IMAGE_TAG: "16",
-  POSTGRES_PORT: config.ports.db.postgres,
+  POSTGRES_PORT: config.ports.db.langtrace_postgres,
 
   // App settings
-  NEXT_PUBLIC_APP_NAME: config.services.langtrace.api.host,
+  NEXT_PUBLIC_APP_NAME: `http://localhost:${config.ports.langtrace}/api/trace`,
   NEXT_PUBLIC_ENVIRONMENT: "production",
-  NEXT_PUBLIC_HOST: "http://localhost:3000",
+  NEXT_PUBLIC_HOST: `http://localhost:${config.ports.langtrace}`,
   NEXTAUTH_SECRET: config.services.dashboard.auth.nextauth_secret,
-  NEXTAUTH_URL: "http://localhost:3000",
-  NEXTAUTH_URL_INTERNAL: "http://localhost:3000",
+  NEXTAUTH_URL: `http://localhost:${config.ports.langtrace}`,
+  NEXTAUTH_URL_INTERNAL: `http://localhost:${config.ports.langtrace}`,
 
   // Clickhouse settings
   CLICK_HOUSE_HOST: config.databases.langtrace_clickhouse.host,
